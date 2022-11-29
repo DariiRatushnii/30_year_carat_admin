16c011d8c2acd1eac21f58a466553c51526954e4
DariiRatushnii <Darii.Ratushnii@dentsu.com.ua>
2022-11-29 13:11:56 +0200

    6-7.header and title fix

--- landing/index.php
+++ landing/index.php
@@ -8,38 +8,9 @@ $emplyees = get_emplyees();
 
 <head>
   <!-- Global site tag (gtag.js) - Google Analytics -->
-
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Futurelab 12 | Головна | Dentsu Ukraine</title>
-  <link rel="apple-touch-icon" sizes="57x57" href="./img/favicon/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="./img/favicon/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="./img/favicon/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="./img/favicon/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="./img/favicon/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="./img/favicon/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="./img/favicon/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="./img/favicon/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="./img/favicon/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192" href="./img/favicon/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="./img/favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="./img/favicon/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="./img/favicon/favicon-16x16.png">
-  <link rel="manifest" href="./img/favicon/manifest.json">
-  <meta name="msapplication-TileColor" content="#000">
-  <meta name="msapplication-TileImage" content="./img/favicon/ms-icon-144x144.png">
-  <meta name="theme-color" content="#000">
-
-  <meta property="og:image" content="https://futurelab.dentsu.com.ua/img/FL12_FB_preview.png" />
-  <meta property="og:image:secure_url" content="https://futurelab.dentsu.com.ua/img/FL12_FB_preview.png" />
-  <meta property="og:type" content="website" />
-  <meta property="og:url" content="https://futurelab.dentsu.com.ua" />
-  <meta property="og:title" content="Future Lab – щорічна маркетингова конференція групи dentsu Ukraine з 2011 року." />
-  <meta property="og:description"
-    content="2 вересня 2021. 12-а конференція. Тема року – HumanTech – про синергію комунікацій та ІТ, про martech-рішення, які працюють на благо людини та суспільства." />
-
-
-
+  <title>Вітаємо із 30-річчям CARAT | Dentsu Ukraine</title>
   <link
     href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,900;1,700;1,900&display=swap"
     rel="stylesheet">
